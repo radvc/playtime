807f0569d8461b1d706f77904648a3b955430e08
Ryan Angelo <cadagryanangelo@gmail.com>
2017-10-23 21:11:20 +0800

    add column slug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170908012526) do
+ActiveRecord::Schema.define(version: 20171023124712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 20170908012526) do
     t.text "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
   end
 
   add_foreign_key "pledges", "users"
