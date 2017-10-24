b2d8dec163109d9c7ae0a3f53a869cde03b08b55
Ryan Angelo <cadagryanangelo@gmail.com>
2017-10-25 07:47:22 +0800

    remove route wishlist_by_slug

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,13 @@ Rails.application.routes.draw do
   end
 
   # Wishlists & Items
-  resources :wishlists, except: [:index, :show] do
+  resources :wishlists, except: [:index] do
     resources :wishlist_items, shallow: true,
                                only: [:create, :edit, :update, :destroy]
     resource :amazon_search,   controller: :amazon_search,
                                only: [:new, :show]
   end
   
-  get 'wishlists/:slug', to: 'wishlists#show', as: :wishlist_by_slug
-
   # Users
   resources :users
 
